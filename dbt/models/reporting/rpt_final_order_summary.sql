9feda149daa9e02299b857e5d0099965df77ed77
Paul Nelson <nelson.paulj@gmail.com>
2023-07-19 13:06:35 -0300

    remove unnecessary text

--- dbt/models/reporting/rpt_final_order_summary.sql
+++ dbt/models/reporting/rpt_final_order_summary.sql
@@ -13,28 +13,3 @@ JOIN {{ ref('rpt_top_3_product_categories_by_revenue_by_day') }} top_3_rev
     ON top_3_rev.order_purchase_date = r.order_purchase_date
 JOIN {{ ref('rpt_top_3_product_categories_revenue_percentage_per_day') }} top_3_pct
     ON top_3_pct.order_purchase_date = r.order_purchase_date
-
-
-
--- 1. Create DBT models and lineage that result in and produces the following metrics at order
--- purchase date grain in descending date order:
--- a. Total orders
--- b. Total customers making orders
--- c. Total revenue
--- d. Average revenue per order
--- e. Top 3 product categories by revenue (by day)
--- f. Percent of day’s revenue associated with each of the top 3 product categories
-
-
--- Column Name Format/Type
--- order_purchase_date date
--- orders_count number
--- customers_making_orders_count number - two decimal places
--- revenue_usd number
--- average_revenue_per_order_usd number - two decimal places
--- top_3_product_categories_by_revenue string - comma separated list, in
--- descending order of revenue
-
--- top_3_product_categories_revenue_percentage string - comma separated list of numbers
--- to two decimal places, in descending
--- order of revenue percentage
