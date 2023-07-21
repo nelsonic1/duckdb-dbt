e3cd170d97964c7e58c5bd00f99196a325e0aafa
Paul Nelson <nelson.paulj@gmail.com>
2023-07-21 14:11:26 -0300

    again

--- dbt/models/warehouse/dim/dim_customer.sql
+++ dbt/models/warehouse/dim/dim_customer.sql
@@ -1,6 +1,6 @@
 SELECT
     {{ dbt_utils.generate_surrogate_key(['customer_id']) }} AS customer_key,
-    customer_id,
+customer_id,
     customer_unique_id,
     customer_zip_code_prefix,
     customer_city,
